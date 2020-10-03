8712b86f15a166877d6f1e1713edeb5296143c84
Aiya Siddig <65461462+aiyasiddig@users.noreply.github.com>
2020-10-03 13:16:44 -0700

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+var questions = [
+  {
+    title: "Bootstrap is a framework for which two languages?",
+    choices: ["CSS + JS", "JS + HTML", "MySQL + JS", "JAVA + MySQL"],
+    answer: "CSS + JS"
+  },
+  {
+    title: "What is an array?",
+    choices: ["A data type", "A data structure", "A variable", "A function type"],
+    answer: "A data structure"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "Which values do boolean statements return?",
+    choices: ["True or False", "Stringified data", "Arrays", "Numbers"],
+    answer: "True or False"
+  },
+  {
+    title:
+      "Jquery is used within",
+    choices: ["html", "javascript", "css", "JAVA"],
+    answer: "javascript"
+  }
+];
